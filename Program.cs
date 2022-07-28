66364814fce5bf211c12c4303696dcf49c974570
Vasiliy <vasiliy.fedorov.01@mail.ru>
2022-07-28 20:23:33 +0300

    Создаем цикл с проверкой элементов первого массива и создаем из подходящих по условию второй массив

--- Program.cs
+++ Program.cs
@@ -13,3 +13,15 @@ string line = Console.ReadLine();
 string[] arr1 = line.Split(' ');
 string[] arr2 = new string [arr1.Length];
 int count = 0;
+
+
+int[] arr = new int [arr1.Length];
+
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= 3)
+    {
+        arr2[count] = arr1[i];
+        count++;
+    }
+}
